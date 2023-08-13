74c5099496d8a92ee3a745aa10abab603aba77b2
Sumit <tiwarisumit541@gmail.com>
2023-08-13 14:56:59 +0545

    html and css

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log("hello")
+console.log(1212)
+console.log(true)
+console.log(false)
+console.log('hello')
+
+console.log("Hello ram")
+console.log("Hello Shyam")
+console.log("Hello Hari")
+
+let greeting = "Namaste"
+
+console.log(greeting,"ram")
+console.log(greeting,"Shyam")
+console.log(greeting,"Hari")
+
+let age = "thirty"
+
+console.log("Age is",age)
+
+let married = false
+console.log("married status is",married)
+
+let FirstName ="john"
+
+console.log(FirstName)
+console.log(FirstName)
+console.log(FirstName)
+
+let LastName ="doe"
+ 
+
+let FullName = FirstName + LastName
+
+console.log("FullName is",FullName)
+
+let number1 = 100
+let number2 = 200
+let number3 = number1 +number2
+console.log(number3)
+
+let brand1 = "apple inc"
+let brand2 ="lg"
+
+console.log(brand1)
+console.log(brand2)
+
+let count = 100
+count = 500
+
+console.log("count",count)
